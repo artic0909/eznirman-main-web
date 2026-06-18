6cf273994a7f062d74b1297b8139c92f7403da9d
artic0909 <dev.saklin@gmail.com>
2026-06-18 16:08:00 +0530

    sds

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
             __DIR__ . '/../routes/admin-routes.php',
             __DIR__ . '/../routes/account-routes.php',
         ],
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
