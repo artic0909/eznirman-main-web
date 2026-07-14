50f613c0589a856dfb89ef44a7d207506a21f9c3
artic0909 <dev.saklin@gmail.com>
2026-07-14 12:31:32 +0530

    sfdg

--- resources/views/account/layouts/app.blade.php
+++ resources/views/account/layouts/app.blade.php
@@ -194,14 +194,14 @@
           <li class="dropdown pc-h-item header-user-profile">
             <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button"
               aria-haspopup="false" data-bs-auto-close="outside" aria-expanded="false">
-              <img src="{{ asset('../../assets/images/logo.gif') }}" alt="user-image" class="user-avtar">
+              <img src="{{ asset('logo_with_bg.png') }}" alt="user-image" class="user-avtar">
               <span>{{ Auth::guard('account')->user()->name ?? 'Accountant Panel' }}</span>
             </a>
             <div class="dropdown-menu dropdown-user-profile dropdown-menu-end pc-h-dropdown">
               <div class="dropdown-header">
                 <div class="d-flex mb-1">
                   <div class="flex-shrink-0">
-                    <img src="{{ asset('../../assets/images/logo.gif') }}" alt="user-image" class="user-avtar wid-35">
+                    <img src="{{ asset('logo_with_bg.png') }}" alt="user-image" class="user-avtar wid-35">
                   </div>
                   <div class="flex-grow-1 ms-3">
                     <h6 class="mb-1">{{ Auth::guard('account')->user()->name ?? 'Accountant' }}</h6>
